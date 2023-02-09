faf93d4ed66a81766f4aa25bbfa96521313adda5
black03Panther <henning.jeschke@urs-os.de>
2023-02-09 14:46:07 +0100

    folge1 fertig

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,30 +1,6 @@
-<!doctype html>
-
-<title>Laravel From Scratch Blog</title>
-<link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
-<link rel="preconnect" href="https://fonts.gstatic.com">
-<link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600;700&display=swap" rel="stylesheet">
-
-<body style="font-family: Open Sans, sans-serif">
-    <section class="px-6 py-8">
-        <nav class="md:flex md:justify-between md:items-center">
-            <div>
-                <a href="/">
-                    <img src="./images/logo.svg" alt="Laracasts Logo" width="165" height="16">
-                </a>
-            </div>
-
-            <div class="mt-8 md:mt-0">
-                <a href="/" class="text-xs font-bold uppercase">Home Page</a>
-
-                <a href="#" class="bg-blue-500 ml-3 rounded-full text-xs font-semibold text-white uppercase py-3 px-5">
-                    Subscribe for Updates
-                </a>
-            </div>
-        </nav>
-
-        <header class="max-w-xl mx-auto mt-20 text-center">
-            <h1 class="text-4xl">
+<x-app-layout>
+    <x-slot name="header">
+    <h1 class="text-4xl">
                 Latest <span class="text-blue-500">Laravel From Scratch</span> News
             </h1>
 
@@ -39,7 +15,7 @@
             <div class="space-y-2 lg:space-y-0 lg:space-x-4 mt-8">
                 <!--  Category -->
                 <div class="relative flex lg:inline-flex items-center bg-gray-100 rounded-xl">
-                    <select class="flex-1 appearance-none bg-transparent py-2 pl-3 pr-9 text-sm font-semibold">
+                    <select class="flex-1 appearance-none bg-transparent py-2 pl-3 pr-9 text-sm font-semibold border-0 bg-none">
                         <option value="category" disabled selected>Category
                         </option>
                         <option value="personal">Personal</option>
@@ -59,7 +35,7 @@
 
                 <!-- Other Filters -->
                 <div class="relative flex lg:inline-flex items-center bg-gray-100 rounded-xl">
-                    <select class="flex-1 appearance-none bg-transparent py-2 pl-3 pr-9 text-sm font-semibold">
+                    <select class="flex-1 appearance-none bg-transparent py-2 pl-3 pr-9 text-sm font-semibold border-0 bg-none">
                         <option value="category" disabled selected>Other Filters
                         </option>
                         <option value="foo">Foo
@@ -83,13 +59,13 @@
                 <div class="relative flex lg:inline-flex items-center bg-gray-100 rounded-xl px-3 py-2">
                     <form method="GET" action="#">
                         <input type="text" name="search" placeholder="Find something"
-                               class="bg-transparent placeholder-black font-semibold text-sm">
+                               class="bg-transparent placeholder-black font-semibold text-sm border-0 py-0">
                     </form>
                 </div>
             </div>
-        </header>
+    </x-slot>
 
-        <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
+   
             <article
                 class="transition-colors duration-300 hover:bg-gray-100 border border-black border-opacity-0 hover:border-opacity-5 rounded-xl">
                 <div class="py-6 px-5 lg:flex">
@@ -110,9 +86,11 @@
                             </div>
 
                             <div class="mt-4">
+                                <a href="{{url('post')}}">
                                 <h1 class="text-3xl">
                                     This is a big title and it will look great on two or even three lines. Wooohoo!
                                 </h1>
+</a>
 
                                 <span class="mt-2 block text-gray-400 text-xs">
                                         Published <time>1 day ago</time>
@@ -464,35 +442,7 @@
                         </div>
                     </div>
                 </article>
-            </div>
-        </main>
+</div>
 
-        <footer class="bg-gray-100 border border-black border-opacity-5 rounded-xl text-center py-16 px-10 mt-16">
-            <img src="./images/lary-newsletter-icon.svg" alt="" class="mx-auto -mb-6" style="width: 145px;">
-            <h5 class="text-3xl">Stay in touch with the latest posts</h5>
-            <p class="text-sm mt-3">Promise to keep the inbox clean. No bugs.</p>
-
-            <div class="mt-10">
-                <div class="relative inline-block mx-auto lg:bg-gray-200 rounded-full">
-
-                    <form method="POST" action="#" class="lg:flex text-sm">
-                        <div class="lg:py-3 lg:px-5 flex items-center">
-                            <label for="email" class="hidden lg:inline-block">
-                                <img src="./images/mailbox-icon.svg" alt="mailbox letter">
-                            </label>
-
-                            <input id="email" type="text" placeholder="Your email address"
-                                   class="lg:bg-transparent py-2 lg:py-0 pl-4 focus-within:outline-none">
-                        </div>
-
-                        <button type="submit"
-                                class="transition-colors duration-300 bg-blue-500 hover:bg-blue-600 mt-4 lg:mt-0 lg:ml-3 rounded-full text-xs font-semibold text-white uppercase py-3 px-8"
-                        >
-                            Subscribe
-                        </button>
-                    </form>
-                </div>
-            </div>
-        </footer>
-    </section>
-</body>
+    
+</x-app-layout>
