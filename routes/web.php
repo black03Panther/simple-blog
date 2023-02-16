be923bb697f02427f2684e06de74e5e717121d88
black03Panther <henning.jeschke@urs-os.de>
2023-02-16 14:50:02 +0100

    3.1

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
+use App\Models\Post;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,7 +17,10 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::get('/', function () {
-    return view('home');
+
+    $posts = Post::all();
+    //dd($posts);
+    return view('home',['posts' => $posts]);
 });
 
 Route::get('/post', function () {
